5efe0d647f0cfe97d1ae4ad105967bc754d9783c
Mitchell Manar <mitchell.manar@lessannoyingcrm.com>
2024-02-26 12:01:00 -0600

    start object style

---
+++ object.php
@@ -0,0 +1,54 @@
+<?php
+
+abstract class BulkRunner {
+	protected $UserId;
+	protected $SearchType;
+	protected $SearchTerms;
+	protected $RecordIds;
+
+	public function __construct($UserId, $SearchType, $SearchTerms, $RecordIds) {
+		$this->UserId = $UserId; 
+		$this->SearchType = $SearchType; 
+		$this->SearchTerms = $SearchTerms; 
+		$this->RecordIds = $RecordIds; 
+	}
+
+	public function GetContactIds($NumberOfContacts) {
+		if ($this->SearchType === 'ContactIds') {
+			$Numbers = range(0, 100);
+			return array_slice($Numbers, 0, $NumberOfContacts);
+		} else {
+			throw new Exception("not implemented");
+		}
+	}
+
+	// null = not chunked
+	abstract function GetActionChunkSize();
+	abstract function ApplyChanges($RecordIds);
+
+	public function RunAction() {
+		$ContactIds = $this->GetContactIds(10);
+		$RecordChunks = [];
+		$ChunkSize = $this->GetActionChunkSize();
+		if ($ChunkSize) {
+			$RecordChunks = array_chunk($ContactIds, $ChunkSize);
+		} else {
+			// null > no chunking 
+			$RecordChunks = [$ContactIds];
+		}
+
+		foreach($RecordChunks as $Index => $RecordChunk) {
+			echo "start sql transaction $Index \n";
+			$this->ApplyChanges($RecordChunk);
+			echo "end sql transaction $Index  \n";
+		}
+	}
+}
+
+function Main() {
+	$User = "Matchell"; 
+	$RecordType = "Contacts";
+	$CustomDataToUpdate = ['BackgroundInfo' => "bulk updated"];
+	$AllContactIds = BulkActions::GetContactIds(10);
+}
+Main();
